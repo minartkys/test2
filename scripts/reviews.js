b8815a5f8a6a0da48a7f7b1f59d658cf3ef14ee1
minartkys <minart19992@gmail.com>
2023-09-07 09:07:18 +0300

    first commikt

---
+++ scripts/reviews.js
@@ -0,0 +1,23 @@
+const photo1 = document.getElementById('photo1')
+const photo2 = document.getElementById('photo2')
+const photo3 = document.getElementById('photo3')
+const photo4 = document.getElementById('photo4')
+const photo5 = document.getElementById('photo5')
+const photo6 = document.getElementById('photo6')
+const photo7 = document.getElementById('photo7')
+const photo8 = document.getElementById('photo8')
+const photo9 = document.getElementById('photo9')
+const photo10 = document.getElementById('photo10')
+
+photo1.addEventListener('mouseover', () => {
+    photo1.classList.add('photo1')
+})
+photo1.addEventListener('mouseout', () => {
+    photo1.classList.remove('photo1')
+})
+photo2.addEventListener('mouseover', () => {
+    photo2.classList.add('photo2')
+})
+photo2.addEventListener('mouseout', () => {
+    photo2.classList.remove('photo2')
+})
